7c9885f46f33163810eca565bae900a9aac3f580
happymonkey1 <7463843+happymonkey1@users.noreply.github.com>
2025-08-24 21:16:45 -0700

    chore: add kb_std header for easier include on client side

---
+++ include/kb/kb_std.hpp
@@ -0,0 +1,16 @@
+//
+// Created by happy on 8/24/2025.
+//
+
+#ifndef KB_NETWORKING_KB_STD_HPP
+#define KB_NETWORKING_KB_STD_HPP
+
+#include "kb/kb_core.h"
+
+#include "kb/core/types.hpp"
+#include "kb/core/logger.hpp"
+#include "kb/core/owning_buffer.hpp"
+
+#include "kb/mem/arena_allocator.hpp"
+
+#endif  //KB_NETWORKING_KB_STD_HPP
