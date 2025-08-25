245004123dd0cf3df9871a9e2a26a42e31833629
happymonkey1 <7463843+happymonkey1@users.noreply.github.com>
2025-08-24 21:03:25 -0700

    chore: add basic logger test

---
+++ tests/logger_test.cpp
@@ -0,0 +1,16 @@
+//
+// Created by happy on 8/24/2025.
+//
+
+#include "kb/kb_core.h"
+
+auto main([[maybe_unused]] int argc, [[maybe_unused]] char ** argv) -> int {
+
+  kb::core::Logger::init();
+
+  KB_LOG_INFO("Hello from logger test");
+
+  kb::core::Logger::shutdown();
+
+  return 0;
+}
